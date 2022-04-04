e598a4c7a7c2fe9dc9c1e3337adc9798137c7672
Ankur <ankurmishra0043@gmail.com>
2022-04-05 01:31:24 +0530

    Print the nodes at the kth level

---
+++ printing_node_at(k_distance).cpp
@@ -0,0 +1,22 @@
+/*
+//Printing the nodes at k level from the root
+
+void print_dist(Node *root,int k)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    if(k==0)
+    {
+        cout<<(root->key)<<" ";
+    }
+    else
+    {
+        print_dist(root->left,k-1);
+        print_dist(root->right,k-1);
+    }
+}
+//Time complixity is O(n);
+//Auxiliary space is O(h);
+*/
