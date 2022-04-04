780713349de908b962326e97a13114a368ec60a8
Ankur <ankurmishra0043@gmail.com>
2022-04-04 23:24:00 +0530

    The implementation of the preorder and the postorder traversal

---
+++ tree_traversal.cpp
@@ -0,0 +1,59 @@
+/*
+Two types;
+i)Breadth for Search
+ii)Depth for Search
+    a)inorder   ->    (left , root , right)
+    b)preorder  ->    (root , left , right)
+    c)postorder ->    (left , right , root)
+
+*/
+
+/*
+//inorder tree traversal
+void inorder(Node *root)
+{
+    if(root !=NULL)
+    {
+        inorder(root->left);
+        cout<<(root->key)<<" ";
+        inorder(root->right);
+    }
+}
+
+//Time complixity is O(n);
+//Auxiliary space is O(h);
+*/
+
+/*
+//preorder traversal
+void preorder(Node *root)
+{
+    if(root!=NULL)
+    {
+        cout<<(root->key)<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+//Time complixity is O(n);
+//Auxiliary space is O(h);
+
+*/
+
+/*
+//postorder traversal
+void postorder(Node *root)
+{
+    if(root!=NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        cout<<(root->key)<<" ";
+    }
+}
+
+//Time complixity is O(n);
+//Auxiliary Space is O(h);
+
+*/
