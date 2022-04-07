82a2254f4a4c07a945dbd4668b172ccd6f9ff947
Ankur <ankurmishra0043@gmail.com>
2022-04-07 12:07:45 +0530

    The second method for the left view of the binary tree is

--- Tree/Print_leftview.cpp
+++ Tree/Print_leftview.cpp
@@ -21,8 +21,39 @@ void printleftView(Node *root)
 {
     printleft(root,1);
 }
+------------------------------------------
 
-
+//method 2;
+void printleft(Node *root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    queue<Node *>q;
+    q.push(root);
+    while(q.empty()==false)
+    {
+        int count= q.size();
+        for(int i=0;i<count;i++)
+        {
+            Node *curr=q.top();
+            q.pop();
+            if(i==0)
+            {
+                cout<<(curr->key)<<" ";
+            }
+            if(curr->left!=NULL)
+            {
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+    }
+}
 
 
 
